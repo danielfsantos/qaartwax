6579f59a4207e27850a1190ace6334123a81c7a1
Daniel Fernandes <danielfsantos56@gmail.com>
2021-02-06 14:40:51 -0300

    Finalizacao do cenario

--- features/menu_categorias.feature
+++ features/menu_categorias.feature
@@ -4,4 +4,7 @@ Funcionalidade: Validar menus do site artwax
 
     @menu_categorias
     Cenario: Acessar menu categorias
-    Dado que estou na pagina categorias
+    Dado que estou na pagina inicial da artwax
+    Quando selecionar o menu categorias 
+    E selecionar o submenu lixeira automotiva
+    Então a pagina lixeira automotiva e acessada com sucesso
--- features/step_definitions/home_artwax_steps.rb
+++ features/step_definitions/home_artwax_steps.rb
@@ -1,9 +1,18 @@
-Dado(/^que estou na pagina categorias$/) do
-   home_artwax = HomeArtwaxPage.new
-   home_artwax.load
+home_artwax = HomeArtwaxPage.new
 
+Dado(/^que estou na pagina inicial da artwax$/) do
+   home_artwax.load
+end
+
+Quando(/^selecionar o menu categorias$/) do
    home_artwax.menu_categorias.click
+ end
+ 
+ Quando(/^selecionar o submenu lixeira automotiva$/) do
    home_artwax.wait_until_submenu_lixeira_automotiva_visible
    home_artwax.submenu_lixeira_automotiva.click
+ end
+ 
+ Então(/^a pagina lixeira automotiva e acessada com sucesso$/) do
    expect(home_artwax). to have_titulo_page_lixeira_automotiva
-end
+ end
